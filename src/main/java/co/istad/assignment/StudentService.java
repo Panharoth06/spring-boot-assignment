1d2adb7b5057d0dad6ddf1e518c04313e3bfb43d
Panharoth06 <panharothchheng0903@gmail.com>
2025-06-17 22:42:12 +0700

    update: updated score - two decimal places

--- src/main/java/co/istad/assignment/StudentService.java
+++ src/main/java/co/istad/assignment/StudentService.java
@@ -18,7 +18,7 @@ public class StudentService {
         Student student = new Student(
                 studentRepository.getStudentList().size() + 1,
                 "kaka",
-                60 + (Math.random() * 41)
+                (double) Math.round ((60 + (Math.random() * 41) * 100.0) / 100.0)
         );
         return studentRepository.add(student);
     }
